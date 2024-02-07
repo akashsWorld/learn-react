422c10e96b8471d15cb3f87cfb7d49361079a323
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-07 19:25:18 +0530

    Add inline styles in runtime

--- 01_style-in-react-app/myapp/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01_style-in-react-app/myapp/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setIsValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            backgroundColor: !isValid ? "rgba(255, 0, 0, 0.4)" : "",
+            border: !isValid ? "2px solid red" : "",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
