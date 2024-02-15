8a50c9cf7c3b4b5530cd39bc616aa96f252bf06f
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-16 01:38:50 +0530

    Manage multiple states using redux.

--- 03_redux_react/react-redux/src/components/Counter.js
+++ 03_redux_react/react-redux/src/components/Counter.js
@@ -1,26 +1,40 @@
-import classes from './Counter.module.css';
-import {useSelector, useDispatch} from 'react-redux';
+import { useState } from "react";
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-
   // Subscribe the store the state props passed autometically when the state is change.
   // Here also can use useStore hook but useSelector is more efficiant.
-  const counter = useSelector(state=>state.counter);
+  const counter = useSelector((state) => state.counter);
+
+  const show = useSelector(state => state.showCounter);
 
   // This hook gives the dispatch function where we just have to send the action prop.
   const dispath = useDispatch();
 
-
-
-  const toggleCounterHandler = () => {};
+  const [changeVal, setChangeVal] = useState(0);
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <button onClick={()=>dispath({type:'increment'})}>Increment</button>
-      <button onClick={()=>dispath({type:'decrement'})}>Decrement</button>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      {show && <div className={classes.value} >{counter}</div>}
+      <button onClick={() => dispath({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispath({ type: "decrement" })}>Decrement</button>
+      <input
+        value={changeVal}
+        onChange={(eve) => setChangeVal(+eve.target.value)}
+      />
+      <button
+        onClick={() => dispath({ type: "incrementval", payload: changeVal })}
+      >
+        Increment Value
+      </button>
+      <button
+        onClick={() => dispath({ type: "decrementval", payload: changeVal })}
+      >
+        Decrement Value
+      </button>
+      <button onClick={()=>dispath({type:'toggle'})}>Toggle Counter</button>
     </main>
   );
 };
--- 03_redux_react/react-redux/src/store/store.js
+++ 03_redux_react/react-redux/src/store/store.js
@@ -1,7 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
 
 const initialState = {
-    counter:0
+    counter:0,
+    showCounter:false
 }
 
 
@@ -9,17 +10,40 @@ const reducer = (state= initialState,action)=>{
 
     if(action.type === 'increment'){
         return {
-            counter: state.counter+1
+            counter: state.counter+1,
+            showCounter: state.showCounter
         }
     }
     if(action.type === 'decrement'){
         return{
-            counter: state.counter -1
+            counter: state.counter -1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if(action.type === 'incrementval'){
+        return{
+            counter: state.counter + action.payload,
+            showCounter: state.showCounter
+        }
+    }
+
+    if(action.type === 'decrementval'){
+        return{
+            counter: state.counter - action.payload,
+            showCounter: state.showCounter
+        }
+    }
+    if(action.type=== 'toggle'){
+        return {
+            counter:state.counter,
+            showCounter: !state.showCounter
         }
     }
 
     return {
-        counter: state.counter
+        counter: state.counter,
+        showCounter:state.showCounter
     }
 }
 
